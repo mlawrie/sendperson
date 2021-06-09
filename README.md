0498f5e64a3db7f404b764bfb799176f541578ba
Michael Lawrie <michael@michaellawrie.com>
2021-06-09 00:09:29 -0500

    Updates readme; begins to implement request bodies UI

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # sendperson
 
-
-
 ## Development
 
 ```
@@ -9,7 +7,31 @@ yarn install
 yarn start
 ```
 
+## Planned features
+
+#### 1.0 Release
+1. ~~App scaffolding~~
+   1. ~~Show a window~~
+   1. ~~Save preferences~~
+1. Request UI
+   1. ~~Query params~~
+   1. ~~Headers~~
+   1. Request bodies _(in progress)_
+   1. Make it pretty
+1. Response UI
+1. Actually send responses and show results
+1. Save responses and requests
+1. Tabs
+1. Package for distribution: **release waterline**
+#### 1.1 Release   
+1. Collections
+1. Default headers
+1. Import/export collections
+1. Import collections from postman
+1. File uploads
+
 ## Re-compile automatically
+
 To recompile automatically and to allow using electron-reload, run this in a separate terminal:
 
 ```yarn run watch```
--- src/domain/Request.ts
+++ src/domain/Request.ts
@@ -1,5 +1,9 @@
 export const HTTP_METHODS = ['GET', 'PUT', 'POST', 'PATCH', 'DELETE']
+export const BODY_FORMATS = ['none', 'form-data', 'x-www-form-urlencoded', 'text']
+export const BODY_TEXT_FORMATS = ['text/plain', 'text/html', 'text/xml', 'application/json', 'text/javascript']
 export type HttpMethod = typeof HTTP_METHODS[number]
+export type BodyFormat = typeof BODY_FORMATS[number]
+export type BodyTextFormat = typeof BODY_TEXT_FORMATS[number]
 
 export type Param = Readonly<{
   key: string
@@ -13,6 +17,8 @@ export type Request = Readonly<{
   queryParams: Param[]
   method: HttpMethod
   uri: string
+  bodyTextFormat: BodyTextFormat,
+  bodyFormat: BodyFormat
 }>
 
 export const defaultParam = (): Param => ({key: '', description: '', value: '', enabled: true})
@@ -21,5 +27,7 @@ export const defaultRequest = (): Request => ({
   method: 'GET',
   uri: '',
   queryParams: [defaultParam(), defaultParam(), defaultParam()],
-  headers: [defaultParam(), defaultParam(), defaultParam()]
+  headers: [defaultParam(), defaultParam(), defaultParam()],
+  bodyFormat: 'none',
+  bodyTextFormat: 'application/json'
 })
